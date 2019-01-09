8d39ce34270d9ae961e75d960f7ee8149c2fa354
bpoep72 <bpoep72@gmail.com>
2019-01-08 22:40:28 -0500

    Styling and for events and location page

--- app/routes/events.js
+++ app/routes/events.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
+    //get the events from the data store
     return this.store.query('event', {});
   },
 });
