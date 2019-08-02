79e4033833d86b406b76695b2e1aff6bb9a0e97d
bpoep72 <bpoep72@gmail.com>
2019-08-02 17:48:34 -0400

    Fixed the component and routes of the events pages

--- app/routes/events.js
+++ app/routes/events.js
@@ -2,9 +2,14 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    //get the events from the data store
-    return this.store.query('event', {}).then(
-      events => events.sortBy('startTime')
-    );
+      return Ember.RSVP.hash({
+        //get the events from the data store
+        events: this.store.query('event', {}).then( events => events.sortBy('startTime') )
+      });
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    Ember.set(controller, 'events', model.events);
   },
 });
--- app/routes/homepage.js
+++ app/routes/homepage.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+      return Ember.RSVP.hash({
+        //get the events from the data store
+        events: this.store.query('event', {}).then( events => events.sortBy('startTime') )
+      });
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    Ember.set(controller, 'events', model.events);
+  },
 });
