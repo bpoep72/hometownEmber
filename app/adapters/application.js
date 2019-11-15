f55bcf29757505829833f8bf052893d8f8bcc8db
bpoep72 <bpoep72@gmail.com>
2019-11-14 19:16:04 -0500

    making the application use the api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
-  namespace: 'api',
+export default DS.RESTAdapter.extend({
+  namespace: '',
+  host: 'http://localhost:9001'
 });
